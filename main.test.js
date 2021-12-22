6e731b51c4c6d3f32248381042c84416c22898a6
Gray593 <btgray6@gmail.com>
2021-12-22 16:38:09 +0000

    Kata Created

--- main.test.js
+++ main.test.js
@@ -1,11 +1,23 @@
 //👉 Write your tests below here:
-describe
-test("Test If there are 3 new arrays")
-    {
-        const array1 = 
-        {
-
-        }
+import main from "./main.js"
+import members from "./members.js"
 
 
-    }
+    
+const name = ['Ben','Charlotte','Dilyn', 'Eddie','Ivan','Kat','Max','Richard','Scott','Ted']
+const age  = [ 20, 37, 22, 31, 37, 21, 28, 19, 26, 23]
+const id   = [1, 2, 3, 4,  5,6, 7, 8, 9, 10]
+
+const actualName = main(members)[0]
+const actualAge  = main(members)[1]
+const actualID   = main(members)[2]
+
+test("compares name array to expected", function (){
+    expect(name).toStrictEqual(actualName)
+})
+test("compares name array to expected", function (){
+    expect(age).toStrictEqual(actualAge)
+})
+test("compares name array to expected", function (){
+    expect(id).toStrictEqual(actualID)
+})
---
+++ members.js
@@ -0,0 +1,13 @@
+const members = [
+    {id:1,name:"Ben",age:20},
+    {id:2,name:"Dilyn",age:37},
+    {id:3,name:"Kat",age:22},
+    {id:4,name:"Ivan",age:31},
+    {id:5,name:"Max",age:37},
+    {id:6,name:"Scott",age:21},
+    {id:7,name:"Charlotte",age:28},
+    {id:8,name:"Eddie",age:19},
+    {id:9,name:"Richard",age:26},
+    {id:10,name:"Ted",age:23}
+]
+export default members
